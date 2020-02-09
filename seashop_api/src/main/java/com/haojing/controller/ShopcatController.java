531099f0ab6750af232341fc743320da7be6cd58
jiange <1071215950@qq.com>
2020-02-09 17:07:41 +0800

    购物车模块：添加购物车，从购物车中删除

--- seashop_api/src/main/java/com/haojing/controller/ShopcatController.java
+++ seashop_api/src/main/java/com/haojing/controller/ShopcatController.java
@@ -25,6 +25,11 @@ public class ShopcatController {
             HttpServletResponse response) {
 
 
+        if (StringUtils.isBlank(userId)) {
+            return ResponseResult.errorMsg("");
+        }
+
+        System.out.println(shopcartBO);
 
         // TODO 前端用户在登录的情况下，添加商品到购物车，会同时在后端同步购物车到redis缓存
 
@@ -38,6 +43,10 @@ public class ShopcatController {
             @RequestParam String itemSpecId,
             HttpServletRequest request,
             HttpServletResponse response) {
+        if (StringUtils.isBlank(userId) || StringUtils.isBlank(itemSpecId)) {
+            return ResponseResult.errorMsg("参数不能为空");
+        }
+
         // TODO 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的商品
 
         return ResponseResult.ok();
