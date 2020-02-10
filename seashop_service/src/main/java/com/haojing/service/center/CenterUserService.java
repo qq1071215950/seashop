380f3fb1a26ee1f14b985b3380f9076c16920a47
jiange <1071215950@qq.com>
2020-02-10 13:13:30 +0800

    用户个人中心：用户信息修改，用户头像上传。

--- seashop_service/src/main/java/com/haojing/service/center/CenterUserService.java
+++ seashop_service/src/main/java/com/haojing/service/center/CenterUserService.java
@@ -1,5 +1,6 @@
 package com.haojing.service.center;
 
+import com.haojing.bo.center.CenterUserBO;
 import com.haojing.entity.Users;
 
 public interface CenterUserService {
@@ -10,4 +11,22 @@ public interface CenterUserService {
      * @return
      */
     Users queryUserInfo(String userId);
+
+
+    /**
+     * 修改用户中心的个人信息
+     * @param userId
+     * @param centerUserBO
+     * @return
+     */
+    Users updateUserInfo(String userId, CenterUserBO centerUserBO);
+
+
+    /**
+     * 用户头像信息
+     * @param userId
+     * @param faceUrl
+     * @return
+     */
+    Users updateUserFace(String userId, String faceUrl);
 }
